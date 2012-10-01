40bfced397466e76f2f0f2f53c52129ba7055fce
Julius.Mazionis <julius.mazionis@gmail.com>
2012-10-02 01:52:33 +0300

    Repo fix

---
+++ VirtualMachineEmulator/VirtualMachineEmulator/Form1.Designer.cs
@@ -0,0 +1,315 @@
+﻿namespace VirtualMachineEmulator
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.enterInputButton = new System.Windows.Forms.Button();
+            this.VirtualMemoryGroupBox = new System.Windows.Forms.GroupBox();
+            this.VirtualMemoryGridView = new System.Windows.Forms.DataGridView();
+            this.RegisterGroupBox = new System.Windows.Forms.GroupBox();
+            this.labelPTR = new System.Windows.Forms.Label();
+            this.labelSF = new System.Windows.Forms.Label();
+            this.labelPC = new System.Windows.Forms.Label();
+            this.labelCX = new System.Windows.Forms.Label();
+            this.labelAX = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.RealMemoryGridView = new System.Windows.Forms.DataGridView();
+            this.ParamGroupBox = new System.Windows.Forms.GroupBox();
+            this.executeAllButton = new System.Windows.Forms.Button();
+            this.executeNextButton = new System.Windows.Forms.Button();
+            this.nextCommandLabel = new System.Windows.Forms.Label();
+            this.taskNameLabel = new System.Windows.Forms.Label();
+            this.openTaskButton = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.outputGroupBox = new System.Windows.Forms.GroupBox();
+            this.outputTextBox = new System.Windows.Forms.TextBox();
+            this.VirtualMemoryGroupBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.VirtualMemoryGridView)).BeginInit();
+            this.RegisterGroupBox.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.RealMemoryGridView)).BeginInit();
+            this.ParamGroupBox.SuspendLayout();
+            this.outputGroupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.BackColor = System.Drawing.Color.White;
+            this.inputTextBox.Enabled = false;
+            this.inputTextBox.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.inputTextBox.Location = new System.Drawing.Point(19, 393);
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(387, 20);
+            this.inputTextBox.TabIndex = 0;
+            // 
+            // enterInputButton
+            // 
+            this.enterInputButton.Enabled = false;
+            this.enterInputButton.Location = new System.Drawing.Point(412, 393);
+            this.enterInputButton.Name = "enterInputButton";
+            this.enterInputButton.Size = new System.Drawing.Size(75, 23);
+            this.enterInputButton.TabIndex = 1;
+            this.enterInputButton.Text = "Enter";
+            this.enterInputButton.UseVisualStyleBackColor = true;
+            this.enterInputButton.Click += new System.EventHandler(this.enterInputButton_Click);
+            // 
+            // VirtualMemoryGroupBox
+            // 
+            this.VirtualMemoryGroupBox.Controls.Add(this.VirtualMemoryGridView);
+            this.VirtualMemoryGroupBox.Location = new System.Drawing.Point(12, 18);
+            this.VirtualMemoryGroupBox.Name = "VirtualMemoryGroupBox";
+            this.VirtualMemoryGroupBox.Size = new System.Drawing.Size(625, 179);
+            this.VirtualMemoryGroupBox.TabIndex = 2;
+            this.VirtualMemoryGroupBox.TabStop = false;
+            this.VirtualMemoryGroupBox.Text = "Virtual Memory";
+            // 
+            // VirtualMemoryGridView
+            // 
+            this.VirtualMemoryGridView.AllowUserToAddRows = false;
+            this.VirtualMemoryGridView.AllowUserToDeleteRows = false;
+            this.VirtualMemoryGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.VirtualMemoryGridView.Location = new System.Drawing.Point(7, 19);
+            this.VirtualMemoryGridView.Name = "VirtualMemoryGridView";
+            this.VirtualMemoryGridView.ReadOnly = true;
+            this.VirtualMemoryGridView.Size = new System.Drawing.Size(612, 154);
+            this.VirtualMemoryGridView.TabIndex = 0;
+            // 
+            // RegisterGroupBox
+            // 
+            this.RegisterGroupBox.Controls.Add(this.labelPTR);
+            this.RegisterGroupBox.Controls.Add(this.labelSF);
+            this.RegisterGroupBox.Controls.Add(this.labelPC);
+            this.RegisterGroupBox.Controls.Add(this.labelCX);
+            this.RegisterGroupBox.Controls.Add(this.labelAX);
+            this.RegisterGroupBox.Location = new System.Drawing.Point(643, 18);
+            this.RegisterGroupBox.Name = "RegisterGroupBox";
+            this.RegisterGroupBox.Size = new System.Drawing.Size(353, 53);
+            this.RegisterGroupBox.TabIndex = 3;
+            this.RegisterGroupBox.TabStop = false;
+            this.RegisterGroupBox.Text = "Registers";
+            // 
+            // labelPTR
+            // 
+            this.labelPTR.AutoSize = true;
+            this.labelPTR.Location = new System.Drawing.Point(290, 25);
+            this.labelPTR.Name = "labelPTR";
+            this.labelPTR.Size = new System.Drawing.Size(10, 13);
+            this.labelPTR.TabIndex = 4;
+            this.labelPTR.Text = "-";
+            // 
+            // labelSF
+            // 
+            this.labelSF.AutoSize = true;
+            this.labelSF.Location = new System.Drawing.Point(246, 25);
+            this.labelSF.Name = "labelSF";
+            this.labelSF.Size = new System.Drawing.Size(10, 13);
+            this.labelSF.TabIndex = 3;
+            this.labelSF.Text = "-";
+            // 
+            // labelPC
+            // 
+            this.labelPC.AutoSize = true;
+            this.labelPC.Location = new System.Drawing.Point(178, 25);
+            this.labelPC.Name = "labelPC";
+            this.labelPC.Size = new System.Drawing.Size(10, 13);
+            this.labelPC.TabIndex = 2;
+            this.labelPC.Text = "-";
+            // 
+            // labelCX
+            // 
+            this.labelCX.AutoSize = true;
+            this.labelCX.Location = new System.Drawing.Point(110, 25);
+            this.labelCX.Name = "labelCX";
+            this.labelCX.Size = new System.Drawing.Size(10, 13);
+            this.labelCX.TabIndex = 1;
+            this.labelCX.Text = "-";
+            // 
+            // labelAX
+            // 
+            this.labelAX.AutoSize = true;
+            this.labelAX.Location = new System.Drawing.Point(16, 25);
+            this.labelAX.Name = "labelAX";
+            this.labelAX.Size = new System.Drawing.Size(10, 13);
+            this.labelAX.TabIndex = 0;
+            this.labelAX.Text = "-";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.RealMemoryGridView);
+            this.groupBox1.Location = new System.Drawing.Point(12, 203);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(625, 174);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Real Memory";
+            // 
+            // RealMemoryGridView
+            // 
+            this.RealMemoryGridView.AllowUserToAddRows = false;
+            this.RealMemoryGridView.AllowUserToDeleteRows = false;
+            this.RealMemoryGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.RealMemoryGridView.Location = new System.Drawing.Point(7, 13);
+            this.RealMemoryGridView.Name = "RealMemoryGridView";
+            this.RealMemoryGridView.Size = new System.Drawing.Size(612, 155);
+            this.RealMemoryGridView.TabIndex = 0;
+            // 
+            // ParamGroupBox
+            // 
+            this.ParamGroupBox.Controls.Add(this.executeAllButton);
+            this.ParamGroupBox.Controls.Add(this.executeNextButton);
+            this.ParamGroupBox.Controls.Add(this.nextCommandLabel);
+            this.ParamGroupBox.Controls.Add(this.taskNameLabel);
+            this.ParamGroupBox.Controls.Add(this.openTaskButton);
+            this.ParamGroupBox.Location = new System.Drawing.Point(643, 78);
+            this.ParamGroupBox.Name = "ParamGroupBox";
+            this.ParamGroupBox.Size = new System.Drawing.Size(352, 119);
+            this.ParamGroupBox.TabIndex = 5;
+            this.ParamGroupBox.TabStop = false;
+            this.ParamGroupBox.Text = "Parameters";
+            // 
+            // executeAllButton
+            // 
+            this.executeAllButton.Enabled = false;
+            this.executeAllButton.Location = new System.Drawing.Point(263, 78);
+            this.executeAllButton.Name = "executeAllButton";
+            this.executeAllButton.Size = new System.Drawing.Size(75, 23);
+            this.executeAllButton.TabIndex = 4;
+            this.executeAllButton.Text = "Execute All";
+            this.executeAllButton.UseVisualStyleBackColor = true;
+            this.executeAllButton.Click += new System.EventHandler(this.executeAllButton_Click);
+            // 
+            // executeNextButton
+            // 
+            this.executeNextButton.Enabled = false;
+            this.executeNextButton.Location = new System.Drawing.Point(263, 48);
+            this.executeNextButton.Name = "executeNextButton";
+            this.executeNextButton.Size = new System.Drawing.Size(75, 23);
+            this.executeNextButton.TabIndex = 3;
+            this.executeNextButton.Text = "Execute";
+            this.executeNextButton.UseVisualStyleBackColor = true;
+            this.executeNextButton.Click += new System.EventHandler(this.executeNextButton_Click);
+            // 
+            // nextCommandLabel
+            // 
+            this.nextCommandLabel.AutoSize = true;
+            this.nextCommandLabel.Location = new System.Drawing.Point(16, 53);
+            this.nextCommandLabel.Name = "nextCommandLabel";
+            this.nextCommandLabel.Size = new System.Drawing.Size(126, 13);
+            this.nextCommandLabel.TabIndex = 2;
+            this.nextCommandLabel.Text = "Next command = <none>";
+            // 
+            // taskNameLabel
+            // 
+            this.taskNameLabel.AutoSize = true;
+            this.taskNameLabel.Location = new System.Drawing.Point(16, 24);
+            this.taskNameLabel.Name = "taskNameLabel";
+            this.taskNameLabel.Size = new System.Drawing.Size(112, 13);
+            this.taskNameLabel.TabIndex = 1;
+            this.taskNameLabel.Text = "Current task = <none>";
+            // 
+            // openTaskButton
+            // 
+            this.openTaskButton.Location = new System.Drawing.Point(263, 19);
+            this.openTaskButton.Name = "openTaskButton";
+            this.openTaskButton.Size = new System.Drawing.Size(75, 23);
+            this.openTaskButton.TabIndex = 0;
+            this.openTaskButton.Text = "Open Task";
+            this.openTaskButton.UseVisualStyleBackColor = true;
+            this.openTaskButton.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // outputGroupBox
+            // 
+            this.outputGroupBox.Controls.Add(this.outputTextBox);
+            this.outputGroupBox.Location = new System.Drawing.Point(643, 203);
+            this.outputGroupBox.Name = "outputGroupBox";
+            this.outputGroupBox.Size = new System.Drawing.Size(352, 174);
+            this.outputGroupBox.TabIndex = 6;
+            this.outputGroupBox.TabStop = false;
+            this.outputGroupBox.Text = "Output";
+            // 
+            // outputTextBox
+            // 
+            this.outputTextBox.BackColor = System.Drawing.Color.Black;
+            this.outputTextBox.ForeColor = System.Drawing.Color.White;
+            this.outputTextBox.Location = new System.Drawing.Point(7, 19);
+            this.outputTextBox.Multiline = true;
+            this.outputTextBox.Name = "outputTextBox";
+            this.outputTextBox.ReadOnly = true;
+            this.outputTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.outputTextBox.Size = new System.Drawing.Size(339, 149);
+            this.outputTextBox.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1044, 474);
+            this.Controls.Add(this.outputGroupBox);
+            this.Controls.Add(this.ParamGroupBox);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.RegisterGroupBox);
+            this.Controls.Add(this.VirtualMemoryGroupBox);
+            this.Controls.Add(this.enterInputButton);
+            this.Controls.Add(this.inputTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.VirtualMemoryGroupBox.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.VirtualMemoryGridView)).EndInit();
+            this.RegisterGroupBox.ResumeLayout(false);
+            this.RegisterGroupBox.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.RealMemoryGridView)).EndInit();
+            this.ParamGroupBox.ResumeLayout(false);
+            this.ParamGroupBox.PerformLayout();
+            this.outputGroupBox.ResumeLayout(false);
+            this.outputGroupBox.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox inputTextBox;
+        private System.Windows.Forms.Button enterInputButton;
+        private System.Windows.Forms.GroupBox VirtualMemoryGroupBox;
+        private System.Windows.Forms.GroupBox RegisterGroupBox;
+        private System.Windows.Forms.Label labelSF;
+        private System.Windows.Forms.Label labelPC;
+        private System.Windows.Forms.Label labelCX;
+        private System.Windows.Forms.Label labelAX;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView VirtualMemoryGridView;
+        private System.Windows.Forms.GroupBox ParamGroupBox;
+        private System.Windows.Forms.Button openTaskButton;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label taskNameLabel;
+        private System.Windows.Forms.Button executeNextButton;
+        private System.Windows.Forms.Label nextCommandLabel;
+        private System.Windows.Forms.GroupBox outputGroupBox;
+        private System.Windows.Forms.Button executeAllButton;
+        private System.Windows.Forms.TextBox outputTextBox;
+        private System.Windows.Forms.DataGridView RealMemoryGridView;
+        private System.Windows.Forms.Label labelPTR;
+    }
+}
+
---
+++ VirtualMachineEmulator/VirtualMachineEmulator/Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace VirtualMachineEmulator
+{
+    public partial class Form1 : Form
+    {
+        private VirtualMachine vm;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            try
+            {
+                Application.Run(new Form1());
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+            }
+        }
+        private void FillVirtualMemory()
+        {
+            VirtualMemoryGridView.Rows.Clear();
+            VirtualMemoryGridView.Columns.Clear();
+            Memory mem = vm.Memory;
+            for (Int16 i = 0; i < mem.WordCount; i++)
+            {
+                VirtualMemoryGridView.Columns.Add(i.ToString("X"), i.ToString("X"));
+                VirtualMemoryGridView.Columns[i].DisplayIndex = i;
+            }
+            for (Int16 i = 0; i < mem.BlockCount; i++)
+            {
+                string[] rowValues = new string[mem.WordCount];
+                for (Int16 j = 0; j < mem.WordCount; j++)
+                {
+                    if(!(mem[i,j].ToString() == "----"))
+                        rowValues[j] = mem[i, j].ToString();
+                }
+                VirtualMemoryGridView.Rows.Add(rowValues);
+                VirtualMemoryGridView.Rows[i].HeaderCell.Value = i.ToString("X");
+
+            }
+        }
+
+        private void FillVirtualMemoryGrid()
+        {
+            Memory mem = vm.Memory;
+            for (Int16 i = 0; i < mem.BlockCount; i++)
+            {
+                string[] rowValues = new string[mem.WordCount];
+                for (Int16 j = 0; j < mem.WordCount; j++)
+                {
+                    if (!(mem[i, j].ToString() == "----"))
+                        rowValues[j] = mem[i, j].ToString();
+                }
+                VirtualMemoryGridView.Rows[i].SetValues(rowValues);
+                VirtualMemoryGridView.Rows[i].HeaderCell.Value = i.ToString("X");
+
+            }
+        }
+
+        private void FillRealMemory()
+        {
+            RealMemoryGridView.Rows.Clear();
+            RealMemoryGridView.Columns.Clear();
+            for (Int16 i = 0; i < RealMachine.Memory.WordCount; i++)
+            {
+                RealMemoryGridView.Columns.Add(i.ToString("X"), i.ToString("X"));
+                RealMemoryGridView.Columns[i].DisplayIndex = i;
+            }
+            for (Int16 i = 0; i < RealMachine.Memory.BlockCount; i++)
+            {
+                string[] rowValues = new string[RealMachine.Memory.WordCount];
+                for (Int16 j = 0; j < RealMachine.Memory.WordCount; j++)
+                {
+                    if (!(RealMachine.Memory[i, j].ToString() == "----"))
+                        rowValues[j] = RealMachine.Memory[i, j].ToString();
+                }
+                RealMemoryGridView.Rows.Add(rowValues);
+                RealMemoryGridView.Rows[i].HeaderCell.Value = i.ToString("X");
+            }
+        }
+
+        private void FillRealMemoryGrid()
+        {
+            vm.MapBlocks();
+            for (Int16 i = 0; i < RealMachine.Memory.BlockCount; i++)
+            {
+                string[] rowValues = new string[RealMachine.Memory.WordCount];
+                for (Int16 j = 0; j < RealMachine.Memory.WordCount; j++)
+                {
+                    if (!(RealMachine.Memory[i, j].ToString() == "----"))
+                        rowValues[j] = RealMachine.Memory[i, j].ToString();
+                }
+                RealMemoryGridView.Rows[i].SetValues(rowValues);
+                RealMemoryGridView.Rows[i].HeaderCell.Value = i.ToString("X");
+            }
+        }
+
+        private void PrepareForInput()
+        {
+            inputTextBox.Enabled = true;
+            executeNextButton.Enabled = false;
+            executeAllButton.Enabled = false;
+            enterInputButton.Enabled = true;
+        }
+
+        private void PrInt16Buffer()
+        {
+            outputTextBox.Text += vm.Io.Buffer;
+            outputTextBox.Text += "\r\n";
+        }
+
+        private void FillRegisters()
+        {
+            labelAX.Text = "AX = " + vm.Cpu.AX.ToString();
+            labelCX.Text = "CX = " + vm.Cpu.CX.ToString();
+            labelPC.Text = "PC = " + vm.Cpu.PC.ToString("X");
+            labelSF.Text = "SF = " + vm.Cpu.SF.ToString();
+            labelPTR.Text = "PTR = " + RealMachine.PTR.ToString("X");
+        }
+
+        private void SetNextCommandLabel()
+        {
+            nextCommandLabel.Text = "Next command = " + vm.Cpu.NextCommand();
+        }
+
+        private void vmTaskFinishHandler()
+        {
+            executeAllButton.Enabled = false;
+            executeNextButton.Enabled = false;
+            nextCommandLabel.Text = "Next command = <none>";
+            taskNameLabel.Text = "Current task = <none>";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //MessageBox.Show(Word.HexToInt("FFFF").ToString());
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                vm = new VirtualMachine(openFileDialog1.FileName);
+                executeNextButton.Enabled = true;
+                executeAllButton.Enabled = true;
+                FillVirtualMemory();
+                FillRealMemory();
+                FillRegisters();
+                SetNextCommandLabel();
+                outputTextBox.Text = String.Empty;
+                taskNameLabel.Text = "Current task = " + openFileDialog1.FileName.Split('\\').Last();
+                this.vm.Cpu.CommandExecuted += new VMEventHandler(FillRegisters);
+                this.vm.Cpu.CommandExecuted += new VMEventHandler(FillVirtualMemoryGrid);
+                this.vm.Cpu.CommandExecuted += new VMEventHandler(FillRealMemoryGrid);
+                this.vm.Cpu.CommandExecuted += new VMEventHandler(SetNextCommandLabel);
+                this.vm.Io.InputRequested += new VMEventHandler(PrepareForInput);
+                this.vm.Io.OutputRequested += new VMEventHandler(PrInt16Buffer);
+                this.vm.VMTaskFinished += new VMEventHandler(vmTaskFinishHandler);
+                this.vm.CheckIfInputNext();
+            }
+        }
+
+        private void executeNextButton_Click(object sender, EventArgs e)
+        {
+            this.vm.ExecuteNext();
+        }
+
+        private void enterInputButton_Click(object sender, EventArgs e)
+        {
+            if (inputTextBox.Text == String.Empty)
+                return;
+            this.vm.Io.Buffer = inputTextBox.Text;
+            executeAllButton.Enabled = true;
+            executeNextButton.Enabled = true;
+            inputTextBox.Enabled = false;
+            enterInputButton.Enabled = false;
+            inputTextBox.Text = String.Empty;
+            this.vm.ExecuteNext();
+        }
+
+        private void executeAllButton_Click(object sender, EventArgs e)
+        {
+            this.vm.ExecuteAll();
+        }  
+    }
+}
